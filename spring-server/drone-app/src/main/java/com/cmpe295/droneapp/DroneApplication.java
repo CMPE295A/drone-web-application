ac36b873d24b322b52d3fbb34e045797eeef2eea
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-05-26 16:48:51 -0700

    rename DroneAppApplication to DroneApplication

--- spring-server/drone-app/src/main/java/com/cmpe295/droneapp/DroneApplication.java
+++ spring-server/drone-app/src/main/java/com/cmpe295/droneapp/DroneApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DroneAppApplication {
+public class DroneApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DroneAppApplication.class, args);
+		SpringApplication.run(DroneApplication.class, args);
 	}
 
 }
