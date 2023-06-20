9bf31db2b669fa89c58cbc6f952925d013eb382a
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-06-20 14:55:00 -0700

    change icons styling; change files extension to jsx

--- client/src/components/gps/DroneMetricsProp.jsx
+++ client/src/components/gps/DroneMetricsProp.jsx
@@ -10,9 +10,9 @@ function DroneMetricsProp(props) {
     const { altitude, speed, distance, temperature } = props;
     let tempIcon;
     if (temperature < 0) {
-        tempIcon = <AcUnitIcon />;
+        tempIcon = <AcUnitIcon style={{ color: '#87CEEB' }} />;
     } else if (temperature >= 0 && temperature < 30) {
-        tempIcon = <WbSunnyIcon />;
+        tempIcon = <WbSunnyIcon style={{ color: '#bc5090' }} />;
     } else {
         tempIcon = <LocalFireDepartmentIcon style={{ color: 'red' }} />;
     }
@@ -21,21 +21,21 @@ function DroneMetricsProp(props) {
 
             <div className='stat-item'>
                 <div className="icon-with-number">
-                    <ArrowUpwardIcon />
+                    <ArrowUpwardIcon style={{ color: '#2196f3' }} />
                     <span>{altitude ? altitude + ' m' : 'Loading...'}</span>
                 </div>
             </div>
 
             <div className='stat-item'>
                 <div className="icon-with-number">
-                    <SpeedIcon />
+                    <SpeedIcon style={{ color: '#f44336' }} />
                     <span>{speed ? speed + ' km/h' : 'Loading...'}</span>
                 </div>
             </div>
 
             <div className='stat-item'>
                 <div className="icon-with-number">
-                    <LocationOnIcon />
+                    <LocationOnIcon style={{ color: '#3f51b5' }} />
                     <span>{distance ? distance + ' km' : 'Loading...'}</span>
                 </div>
             </div>
