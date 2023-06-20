dfeb1c613bd06b4dc221d63457159638ffb0d54b
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-06-20 13:17:29 -0700

    update App.js to use createBrowserRouter v6.4 Data API

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  Navigate
+} from "react-router-dom";
 import Register from "./pages/Register/Register";
 import Navbar from "./components/navbar/Navbar";
 import Main from "./pages/Main"
@@ -8,22 +13,79 @@ import Map from "./components/gps/Map";
 import Login from "./pages/Login/Login";
 const App = () => {
 
-  return (
+  const Layout = () => {
+
+    return (
+
       <div>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Main/>}/>
-              <Route path="/register" element={<Register/>}/>
-              <Route path="/login" element={<Login/>}/>
-              <Route path="/navbar" element={<Navbar/>}/>
-              <Route path="/leftbar" element={<LeftBar/>}/>
-              <Route path="/map" element={<Map/>}/>
+        <Navbar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
 
+          {/* 1/3 space for leftbar and 2/3 space for outlet */}
+          <div style={{ flex: 6 }}>
+            {/* nested routes inside root route */}
+            <Outlet />
+          </div>
+        </div>
 
-          </Routes>
-        </BrowserRouter>
+        {/* <Footer /> */}
+
       </div>
+
+    );
+  };
+
+
+  //client-sidew auth
+  const ProtectedRoute = ({ children }) => {
+    // const { currentUser } = useContext(AuthContext);
+
+    // if (!currentUser) {
+    //   console.log('Not CURRENT USER: ' + currentUser);
+    //   return <Navigate to="/login" />;
+    // }
+
+
+    return children;
+  }
+
+  //define the routes
+  const router = createBrowserRouter([
+    {
+      //root route
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
+      //nested routes
+      children: [
+        {
+          path: "/",
+          element: <Map />,
+        },
+
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
   );
+
+
 }
 
 export default App;
