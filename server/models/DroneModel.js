de6648d8a51ed0d389d8e2b0b855d9bad4fdbafc
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-06-20 22:50:55 -0700

    add targetLocation in drone model, add speed property in gps model

--- server/models/DroneModel.js
+++ server/models/DroneModel.js
@@ -2,18 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const droneSchema = new Schema({
-        droneIdentifier: {type: String, required: true, unique: true},
-        status: {
-            type: String, required: true,
-            enum: ['idle', 'flying'],
-            default: 'idle'
-        },
-        gps: {
-            type: Schema.Types.ObjectId,
-            ref: 'GPS',
-        },
+    droneIdentifier: { type: String, required: true, unique: true },
+    status: {
+        type: String, required: true,
+        enum: ['idle', 'flying', 'offline'],
+        default: 'idle'
     },
-    {timestamps: true}
+    // gps: [gpsSchema],   // array of GPS locations
+    targetLocation: {
+        longitude: { type: Number },
+        latitude: { type: Number }
+    },
+},
+    { timestamps: true }
 );
 
 
--- server/models/gpsModel.js
+++ server/models/gpsModel.js
@@ -2,19 +2,22 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const gpsSchema = new Schema({
-        droneIdentifier: {type: String, required: true, unique: true},
-        latitude: {
-            type: Number,
-            required: true,
-        },
-        longitude: {
-            type: Number,
-            required: true,
-        },
-        altitude: {
-            type: Number,
-        },
-    }, {timestamps: true}
+    droneIdentifier: { type: String, required: true, unique: true },
+    latitude: {
+        type: Number,
+        required: true,
+    },
+    longitude: {
+        type: Number,
+        required: true,
+    },
+    altitude: {
+        type: Number,
+    },
+    speed: {
+        type: Number,
+    }
+}, { timestamps: true }
 );
 
 const gpsModel = mongoose.model('GPS', gpsSchema);
