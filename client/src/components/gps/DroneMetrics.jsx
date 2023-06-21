c769aff597dcfea149b3bc4d751e5f4b1a8b61c9
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-06-20 20:56:52 -0700

    add the GET request for temperature and socket for real time update

--- client/src/components/gps/DroneMetrics.jsx
+++ client/src/components/gps/DroneMetrics.jsx
@@ -1,20 +1,22 @@
 import { useEffect, useState, useContext } from 'react';
 import DroneMetricsProp from './DroneMetricsProp';
+import { backendUrl } from "../../config";
+import axios from "axios";
+import { SocketContext } from '../../contextApi/SocketContext';
 
 const DroneMetrics = () => {
     const [altitudeLevel, setAltitudeLevel] = useState(null);
     const [speedLevel, setSpeedLevel] = useState(20);
     const [distance, setDistance] = useState(20);
-    const [temperature, setTemperature] = useState(31);
+    const [temperature, setTemperature] = useState(41);
     // const [error, setError] = useState(null);
-    // const socket = useContext(SocketContext); //access socket.io connection
+    const socket = useContext(SocketContext); //access socket.io connection
 
 
     const droneIdentifier = "test1";
 
     useEffect(() => {
 
-        //initial battery level to display
         // const getDroneMetrics = async () => {
         //     try {
         //         //get the battery level of the drone
@@ -38,6 +40,36 @@ const DroneMetrics = () => {
 
     }, []);
 
+    useEffect(() => {
+
+        //initial temperature level to display
+        const getTemperatureLevel = async () => {
+            try {
+                //get the battery level of the drone
+                const res = await axios.get(`${backendUrl}/temperature/${droneIdentifier}`);
+                // console.log(res);
+                setTemperature(res.data.temperature);
+            } catch (err) {
+                console.log(err);
+                // setError('Failed to fetch temperature level');
+
+            }
+        };
+
+        getTemperatureLevel();
+
+
+        // real time update temperature level using web socket
+        socket.on('temperatureUpdate', (data) => {
+            // console.log(socket)
+            // console.log(data);
+            if (data.droneIdentifier === 'test1') {
+                setTemperature(data.temperature);
+            }
+        });
+
+
+    }, []);
 
     return (
         <div>
