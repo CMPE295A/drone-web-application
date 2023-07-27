a01e6a7d1834dd4e80955c6488c6d225c13a10ce
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-07-26 20:13:46 -0700

    store the keys to pem files

--- server/messageHandler/keyMessageHandler.js
+++ server/messageHandler/keyMessageHandler.js
@@ -1,4 +1,5 @@
 const crypto = require('crypto');
+const fs = require('fs');
 
 const generateKeyPair = () => {
 
@@ -12,8 +13,16 @@ const generateKeyPair = () => {
     //get generated private key
     const privateKey = ecdh.getPrivateKey();
 
-    console.log('Public key: ', publicKey.toString('base64'));
-    console.log('Private key: ', privateKey.toString('base64'));
+    // Convert keys to base64 strings
+    const publicKeyBase64 = publicKey.toString('base64');
+    const privateKeyBase64 = privateKey.toString('base64');
+
+    console.log('Public key: ', publicKeyBase64);
+    console.log('Private key: ', privateKeyBase64);
+
+    // Write keys to pem files
+    fs.writeFileSync('publicKey.pem', publicKeyBase64);
+    fs.writeFileSync('privateKey.pem', privateKeyBase64);
 }
 
 module.exports = { generateKeyPair }
