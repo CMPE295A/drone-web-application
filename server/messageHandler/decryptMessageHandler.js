6490398d8f79196ffda4308345273e944a179840
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-07-27 19:53:59 -0700

    convert secret from hex to binary

--- server/messageHandler/decryptMessageHandler.js
+++ server/messageHandler/decryptMessageHandler.js
@@ -1,18 +1,22 @@
 const crypto = require('crypto');
+const fs = require('fs');
 
 // to decrypt data
 const decryptMessage = (encryptedMessage) => {
     const algorithm = 'aes-256-cbc'
-    const iv = 'something'
+    const iv = 'abcdefghijklmnop'; // 16 bytes long required
     //load secret key
-    const secretKey = fs.readFileSync('./secretKey.pem');
+    const secretKey = fs.readFileSync('./secretKey.pem', 'utf-8');
+    const secretKey1 = Buffer.from(secretKey, 'hex'); //convert hex to binary
 
 
-    let decipher = crypto.createDecipheriv(algorithm, secretKey, iv); //https://nodejs.org/api/crypto.html#cryptocreatedecipherivalgorithm-key-iv-options
-    let decrypted = decipher.update(encryptedMessage, 'hex', 'utf8'); //input hex, output utf8
+    //secret must be in binary format
+    let decipher = crypto.createDecipheriv(algorithm, secretKey1, iv); //https://nodejs.org/api/crypto.html#cryptocreatedecipherivalgorithm-key-iv-options
+    let decrypted = decipher.update(encryptedMessage, 'hex', 'utf8'); //input buffer, output utf8
 
     //return any remaining decrypted data
     decrypted += decipher.final('utf8');
+
 
     console.log(decrypted);
     return decrypted;
