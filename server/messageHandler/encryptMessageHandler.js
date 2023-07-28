4e1aefac81b20fe7016b575c6d03395e1aa96f55
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-07-28 12:16:03 -0700

    encrypt data in buffer format

--- server/messageHandler/encryptMessageHandler.js
+++ server/messageHandler/encryptMessageHandler.js
@@ -7,25 +7,33 @@ const encryptMessage = (message) => {
 
     const algorithm = 'aes-256-cbc';
     const iv = 'abcdefghijklmnop'; // 16 bytes long required
-    // Load secret key
-    const secretKey = fs.readFileSync('./secretKey.pem', 'utf-8');
-    // const secretKey = crypto.randomBytes(32);
+
+    // const secretKey = crypto.randomBytes(32); // generate key for testing
     // const secretKeyHex = secretKey.toString('hex');
     // fs.writeFileSync('secretKey.pem', secretKey);
-
-    console.log('secret: ' + secretKey);
     // console.log(secretKeyHex);
-    const secretKey1 = Buffer.from(secretKey, 'hex'); //convert hex to binary
 
 
-    //secret key must be in binary format
-    let cipher = crypto.createCipheriv(algorithm, secretKey1, iv); //https://nodejs.org/api/crypto.html#cryptocreatecipherivalgorithm-key-iv-options
+    // Load secret key
+    // const secretKey = fs.readFileSync('./secretKey.pem', 'utf-8'); //mcu generated
+    // const secretKey = fs.readFileSync('./sharedSecret.pem', 'utf-8'); //server generated in hex string
+    const secretKey = fs.readFileSync('./sharedSecret.pem'); //server generated in buffer
+
+    // const secretKeyHex = Buffer.from(secretKey, 'hex'); //convert hex to Buffer if getting hex 
+
+    console.log('shared secret in buffer format: ' + secretKey);
+    // console.log('shared secret in hex format: ' + secretKeyHex);
+
+
+
+    //secret key must be in Buffer format
+    let cipher = crypto.createCipheriv(algorithm, secretKey, iv); //https://nodejs.org/api/crypto.html#cryptocreatecipherivalgorithm-key-iv-options
     let encrypted = cipher.update(message, 'utf8'); //can't be hex
 
     // Return any remaining encrypted data
     encrypted = Buffer.concat([encrypted, cipher.final()]); // Encrypted message 
-    // console.log('encrypted data: ' + encrypted);
-    console.log('encrypted data: ', encrypted.toString('hex'));
+    console.log('encrypted data: ' + encrypted); //output in buffer format
+    // console.log('encrypted data: ', encrypted.toString('hex')); 
 
     return encrypted;
 }
@@ -33,6 +41,3 @@ const encryptMessage = (message) => {
 module.exports = {
     encryptMessage
 }
-// // test
-// const encryptedMessage = encryptMessage('Hello, world!');
-// console.log(encryptedMessage);
