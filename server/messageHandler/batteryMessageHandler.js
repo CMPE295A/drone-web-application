dd87753361f9604b4b4eb37913c92ec928b5a0d6
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-07-28 12:19:11 -0700

    store new drone battery data for analytics instead of updating

--- server/messageHandler/batteryMessageHandler.js
+++ server/messageHandler/batteryMessageHandler.js
@@ -7,18 +7,21 @@ const handleBattery = async (droneIdentifier, message) => {
         // process the message to determine which drone's battery is low
         const batteryLevel = message.batteryLevel;
         console.log('Type of batteryLevel:', typeof batteryLevel);
-        
+
         // Validate the data
         if (typeof batteryLevel !== 'number') {
             throw new Error('Invalid battery data');
         }
 
-        //update battery level
-        const battery = await batteryModel.findOneAndUpdate(
-            { droneIdentifier },
-            { $set: { batteryLevel: batteryLevel } },
-            { new: true }
-        );
+        //create new battery data
+        const battery = new batteryModel({
+            droneIdentifier,
+            batteryLevel,
+        });
+
+        //store to db
+        await battery.save();
+
 
         console.log("battery level:" + battery);
     } catch (err) {
