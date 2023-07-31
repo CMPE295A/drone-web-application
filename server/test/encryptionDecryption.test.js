0bd406df2feff2366d2fc0c4a6bd36f78031284d
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-07-31 16:18:29 -0700

    change the test case for encrypted data to be in hexidecimal format

--- server/test/encryptionDecryption.test.js
+++ server/test/encryptionDecryption.test.js
@@ -6,15 +6,19 @@ const { encryptMessage } = require('../messageHandler/encryptMessageHandler');
 describe('Encryption and Decryption Integration Test', () => {
 
     it('should encrypt and then decrypt back to the original message', function () {
-        const originalMessage = 'Hello, World!';
+        // const originalMessage = 'Hello, World!';
+        const originalMessage = '9';
         console.log('Original message: ' + originalMessage);
         const encryptedMessage = encryptMessage(originalMessage); //encrypt the message in buffer
         const decryptedMessage = decryptMessage(encryptedMessage); // test decrypt
 
         // check if the message is encrypted
         expect(encryptedMessage).to.not.equal(originalMessage);
-        // check if the encrypted message is a Buffer object
-        expect(Buffer.isBuffer(encryptedMessage)).to.be.true;
+
+        // check if the encrypted message is in hexadecimal format
+        expect(typeof encryptedMessage).to.equal('string');
+        expect(/^[0-9a-fA-F]+$/i.test(encryptedMessage)).to.be.true;
+        
         // check if the decrypted message is a string
         expect(decryptedMessage).to.be.a('string');
         // check if the decrypted message is equal to the original message
