45c3b42697814d699b5a2695b7a91859587a11cd
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-08-01 15:54:50 -0700

    refactor to decrypt MQTT message, store drone's status in MongoDB, emit real time updates using websocket

--- server/messageHandler/droneMessageHandler.js
+++ server/messageHandler/droneMessageHandler.js
@@ -1,22 +1,38 @@
 const DroneModel = require('../models/DroneModel');
+const { decryptMessage } = require('../messageHandler/decryptMessageHandler');
 
-//handles MQTT messages received from the broker
-const handleStatus = async (droneIdentifier, message) => {
+
+//handles drone status ['idle', 'flying', 'offline'],
+const handleStatus = (io) => async (droneIdentifier, encryptedMessage) => { //higher-order function
+
     try {
+        const { status } = encryptedMessage;
 
-        const {status} = message;
+        console.log('Encrypted status: ' + status);
 
-        // update drone status in the database
+        //flying
+        //c0f958353c5af040db056247f4d7dd9f
+
+        // offline 
+        // 134ccb987713cbc8b5c346b8d2205d35
+
+
+        //decrypt data
+        let decryptedStatus = decryptMessage(status);
+
+        // update drone status 
         const drone = await DroneModel.findOneAndUpdate(
-            {droneIdentifier},
-            {$set: {status: status}},
-            {new: true}
+            { droneIdentifier },
+            { $set: { status: decryptedStatus } },
+            { new: true, upsert: true } //if no document, create one (update/insert)
         );
-        if (!drone) {
-            throw new Error(`Drone identifier: ${droneIdentifier} not found`);
-        }
 
         console.log("status: " + drone);
+
+        // send a status update event to the client via socket.io
+        io.emit('statusUpdate', { droneIdentifier, status: decryptedStatus });
+
+
     } catch (err) {
         console.error(`Error updating drone status for ${droneIdentifier}:`, err);
     }
