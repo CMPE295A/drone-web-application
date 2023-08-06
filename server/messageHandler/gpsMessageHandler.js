78d26eae8c15bf6790e8d8b77947b35dab4a4b4b
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-08-05 23:56:43 -0700

    add the altitude/speed to the message handler for the gps collection

--- server/messageHandler/gpsMessageHandler.js
+++ server/messageHandler/gpsMessageHandler.js
@@ -4,18 +4,25 @@ const { decryptMessage } = require('../messageHandler/decryptMessageHandler');
 //handles gps data from broker
 const handleGPS = (io) => async (droneIdentifier, encryptedMessage) => {//higher-order function
     try {
-        // const {latitude, longitude, altitude} = message;
-        const { latitude, longitude } = encryptedMessage;
+        // const { latitude, longitude } = encryptedMessage;
+        const { latitude, longitude, altitude, speed } = encryptedMessage;
         console.log('Encrypted {latitude, longitude}: ' + latitude + ', ' + longitude);
+        console.log('Encrypted {altitude, speed}: ' + altitude + ', ' + speed);
 
 
         //decrypt data
         let decryptedLatitude = decryptMessage(latitude);  //e896ed046369845cc3c50e8571cd7e8c
         let decryptedLongitude = decryptMessage(longitude);  //10f4678d15f185427e2911791d751b8f
 
+        let decryptedAltitude = decryptMessage(altitude);
+        let decryptedSpeed = decryptMessage(speed);
+
         // convert decrypted data back into numbers
         decryptedLatitude = parseFloat(decryptedLatitude);
         decryptedLongitude = parseFloat(decryptedLongitude);
+
+        decryptedAltitude = parseFloat(decryptedAltitude);
+        decryptedSpeed = parseFloat(decryptedSpeed);
 
         // // Validate the data
         // if (!latitude || !longitude || !altitude) {
@@ -26,12 +33,23 @@ const handleGPS = (io) => async (droneIdentifier, encryptedMessage) => {//higher
         // Update the GPS data for the specified drone
         const updatedGPSData = await GPS.findOneAndUpdate(
             { droneIdentifier }, // find droneIdentifier
-            // {latitude, longitude, altitude}, // Update the GPS fields
-            { latitude: decryptedLatitude, longitude: decryptedLongitude }, // Update the GPS fields
+            {   // Update the GPS fields
+                latitude: decryptedLatitude,
+                longitude: decryptedLongitude,
+                altitude: decryptedAltitude,
+                speed: decryptedSpeed
+            },
             { new: true, upsert: true } // update if document exists, create a new one if not
         );
 
-        io.emit('locationUpdate', { droneIdentifier, latitude: decryptedLatitude, longitude: decryptedLongitude });
+        // io.emit('locationUpdate', { droneIdentifier, latitude: decryptedLatitude, longitude: decryptedLongitude });
+        io.emit('gpsUpdate', {
+            droneIdentifier,
+            latitude: decryptedLatitude,
+            longitude: decryptedLongitude,
+            altitude: decryptedAltitude,
+            speed: decryptedSpeed
+        });
 
         console.log('GPS data updated:', updatedGPSData);
     } catch (err) {
