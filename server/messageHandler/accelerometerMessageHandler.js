b5641cf8ca3bd8beebb2fe7613ddd5df1a4c79ad
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-08-10 14:48:08 -0700

    decrypt the nested JSON object of accelerometer instead of individually

--- server/messageHandler/accelerometerMessageHandler.js
+++ server/messageHandler/accelerometerMessageHandler.js
@@ -5,17 +5,23 @@ const { decryptMessage } = require('./decryptMessageHandler');
 // accelerometer status from MQTT broker
 const handleAccelerometer = (io) => async (droneIdentifier, encryptedMessage) => { //higher-order function
     try {
-        // process the drone's accelerometer message
-        const { horizontal, vertical, lateral } = encryptedMessage;
+        // // process the drone's accelerometer message (individually encrypted)
+        // const { horizontal, vertical, lateral } = encryptedMessage;
 
+        //nested JSON object encrypted
+        //348426857b487d10ccb749108238dc8014d3b054f194c7d2a9ec46a4cca4cf47a020db3fbad51c42b56fae0d2dc11b846728a7b83206aaa7bd8f13e79b5de058
 
         console.log('Encrypted accelerometer data: ' + encryptedMessage); //ca74ca2f55cbe7f92789a704ff686c2f
 
-        //decrypt data
-        let decryptedHorizontal = decryptMessage(horizontal);
-        let decryptedVertical = decryptMessage(vertical);
-        let decryptedLateral = decryptMessage(lateral);
+        // //if decrypt individual data
+        // let decryptedHorizontal = decryptMessage(horizontal);
+        // let decryptedVertical = decryptMessage(vertical);
+        // let decryptedLateral = decryptMessage(lateral);
 
+        //if decrypt entire nested JSON object. Convert to JS object
+        const decryptedAccelerometer = JSON.parse(decryptMessage(encryptedMessage));
+        let { horizontal: decryptedHorizontal, vertical: decryptedVertical, lateral: decryptedLateral } = decryptedAccelerometer;
+        // console.log(typeof decryptedAccelerometer);
 
         // convert decrypted accelerometer to float
         decryptedHorizontal = parseFloat(decryptedHorizontal);
@@ -27,7 +33,7 @@ const handleAccelerometer = (io) => async (droneIdentifier, encryptedMessage) =>
         if (typeof decryptedHorizontal !== 'number' || typeof decryptedVertical !== 'number' || typeof decryptedLateral !== 'number') {
             throw new Error('Invalid accelerometer data');
         }
-        
+
         //create new accelerometer data
         const newAccelerometer = new accelerometerModel({
             droneIdentifier,
@@ -41,8 +47,8 @@ const handleAccelerometer = (io) => async (droneIdentifier, encryptedMessage) =>
 
 
         //update accelerometer status in real-time on the webpage
-        io.emit('accelerometerUpdate', { 
-            droneIdentifier, 
+        io.emit('accelerometerUpdate', {
+            droneIdentifier,
             horizontal: decryptedHorizontal,
             vertical: decryptedVertical,
             lateral: decryptedLateral
